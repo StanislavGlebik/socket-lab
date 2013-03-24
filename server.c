b3f5817ad95c35d8dbebea5f27d5d16558dca3e9
kolesov93 <kolesov93@gmail.com>
2013-03-24 14:40:51 +0300

    Modifyed server code to get filename and filelength

--- server.c
+++ server.c
@@ -1,8 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
-   process for each connection
- */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,10 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void dostuff(int); /* function prototype */
-void error(const char *msg)
-{
-    perror(msg);
+void acceptFile(int socketHandle); /* function prototype */
+
+void fail(const char* message) {
+    fprintf(stderr, "%s\n", message);
     exit(1);
 }
 
@@ -25,55 +20,75 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
 
     if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
+        fail("Port is not provided");
     }
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    if (sockfd < 0) {
+        fail("Error while opening socket");
+    }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                sizeof(serv_addr)) < 0) 
-        error("ERROR on binding");
-    listen(sockfd,5);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { 
+        fail("Error while binding address to socket");
+    }
+
+    listen(sockfd, 5);
     clilen = sizeof(cli_addr);
     while (1) {
-        newsockfd = accept(sockfd, 
-                (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0) 
-            error("ERROR on accept");
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            fail("Error while accepting connection");
+        }
+
         pid = fork();
-        if (pid < 0)
-            error("ERROR on fork");
+        if (pid < 0) {
+            fail("Error while forking new process");
+        }
+
         if (pid == 0)  {
             close(sockfd);
-            dostuff(newsockfd);
+            acceptFile(newsockfd);
             exit(0);
+        } else { 
+            close(newsockfd);
         }
-        else close(newsockfd);
     } /* end of while */
     close(sockfd);
     return 0; /* we never get here */
 }
 
-/******** DOSTUFF() *********************
-  There is a separate instance of this function 
-  for each connection.  It handles all communication
-  once a connnection has been established.
- *****************************************/
-void dostuff (int sock)
-{
-    int n;
-    char buffer[256];
+void acceptFile(int socketHandle) {
+    int fileLen, nameLen;
+    int bytesRead, bytesWritten;
+    char buffer[10 * 1024];
 
-    bzero(buffer,256);
-    n = read(sock,buffer,255);
-    if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(sock,"I got your message",18);
-    if (n < 0) error("ERROR writing to socket");
+    bytesRead = read(socketHandle, &nameLen, sizeof(nameLen));
+    if (bytesRead != sizeof(nameLen)) {
+        fail("Can't read filename length");
+    }
+    printf("Filename length = %d\n", nameLen);
+
+    bzero(buffer, 10 * 1024);
+    bytesRead = read(socketHandle, buffer, nameLen);
+    if (bytesRead != nameLen) {
+        fail("Can't read filename");
+    }
+    printf("Filename = %s\n", buffer);
+
+    bytesRead = read(socketHandle, &fileLen, sizeof(fileLen));
+    if (bytesRead != sizeof(fileLen)) {
+        fail("Can't read file length");
+    }
+    printf("File length = %d\n", fileLen);
+    
+    bytesWritten = write(socketHandle, "Transmission finished", 23);
+    if (bytesWritten < 0) {
+        fail("Transmission is not finished really. Can't write final message");
+    }
 }
+
