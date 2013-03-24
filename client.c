6bf3bb06a95e73c987ccd9f225fe187268c76d4e
kolesov93 <kolesov93@gmail.com>
2013-03-24 15:24:41 +0300

    Add basic logic to client

--- client.c
+++ client.c
@@ -4,53 +4,86 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
+#define BUFFER_LENGTH (10 * 1024)
+
+
+void fail(const char* message) {
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+int getFileLength(const char* filename) {
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    int sockfd, portno;
+    int file_length, filename_length;
+    int bytesWritten, bytesRead;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+    char buffer[BUFFER_LENGTH];
+    if (argc < 4) {
+        sprintf(buffer, "Usage: %s hostname port filename", argv[0]);
+        fail(buffer);
     }
+
+    if (access(argv[3], F_OK) == -1) {
+        sprintf(buffer, "Error: file %s does not exist", argv[3]);
+        fail(buffer);
+    }
+    filename_length = strlen(argv[3]);
+    file_length = getFileLength(argv[3]);
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+
+    if (sockfd < 0) {
+        fail("Error while opening socket");
+    }
+
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+        fail("Error: no such host");
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-            (char *)&serv_addr.sin_addr.s_addr,
-            server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-        error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-        error("ERROR reading from socket");
+
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+        fail("Error while connecting");
+    }
+
+    bytesWritten = write(sockfd, &filename_length, sizeof(int));
+    if (bytesWritten < 0) { 
+        fail("Error while sending length of filename");
+    }
+
+    bytesWritten = write(sockfd, argv[3], strlen(argv[3]));
+    if (bytesWritten < 0) { 
+        fail("Error while sending filename");
+    }
+
+    bytesWritten = write(sockfd, &file_length, sizeof(int));
+    if (bytesWritten < 0) { 
+        fail("Error while sending length of file");
+    }
+
+    bzero(buffer, BUFFER_LENGTH);
+    bytesRead = read(sockfd, buffer, BUFFER_LENGTH - 1);
+    if (bytesRead < 0) { 
+        fail("Error while getting final message");
+    }
     printf("%s\n",buffer);
     close(sockfd);
     return 0;
